08494dea031b854500f6751e8ab895bf47b10365
Yuri Meitzen <31018558+Yurimeitzen0@users.noreply.github.com>
2021-08-17 10:47:59 -0300

    Desafio 1 completo
    Resolução dos 4 passos do desafio 1.

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,24 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>B. Academy</h1>
+const link = document.querySelector('[data-js="link"]')
+const app = document.querySelector('[data-js="app"]')
+
+document.querySelector('[data-js="app"]').innerHTML = ` <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+
+link.addEventListener('click', () =>
+{
+  event.preventDefault()
+  if(app.style.display == "block")
+  {
+    app.style.display = "none"
+  }
+  else
+  {
+    app.style.display = "block"
+  }
+
+
+
+},false)
