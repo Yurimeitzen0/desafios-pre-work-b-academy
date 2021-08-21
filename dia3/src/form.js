6a46a570dd42072587f166944f5c4bd3716dcc3c
Yuri Meitzen <31018558+Yurimeitzen0@users.noreply.github.com>
2021-08-21 14:49:19 -0300

    Exercicio 2 dia 3.
    Minha resolução da segunda questão.

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -24,3 +24,43 @@ entrada.addEventListener('input',(txt)=>
 txt.target.value = words.join(' ')
 
 })
+
+let colors = new Map([
+  ['vermelho', '#AB0000'],
+  ['amarelo', '#FFC80D'],
+  ['verde', '#22B14B'],
+  ['laranja', '#FF7E26'],
+  ['azul', '#3F47CC']
+]);
+
+
+const indexform = document.querySelector('[data-js="form"]')
+const formselect = document.createElement('select')
+
+const colorbox = document.createElement('div')
+colorbox.style.display = 'flex'
+
+colors.forEach((key,value)=>
+{
+  const option = document.createElement('option')
+  option.value = key
+  option.textContent = value
+  formselect.appendChild(option)
+})
+
+formselect.addEventListener('change', (e)=>
+{
+  colorbox.innerHTML = ''
+  Array.from(e.target.selectedOptions).map(option =>
+    {
+      const div = document.createElement('div')
+      div.style.width = '100px'
+      div.style.height = '100px'
+      div.style.background = option.value
+      colorbox.appendChild(div)
+    })
+})
+
+indexform.appendChild(formselect)
+formselect.setAttribute('multiple', '')
+document.body.appendChild(colorbox)
