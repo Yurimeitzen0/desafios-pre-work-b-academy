a27e747aee1778a253e753fdd1232aa117d0786e
Yuri Meitzen <31018558+Yurimeitzen0@users.noreply.github.com>
2021-08-22 02:23:56 -0300

    Dia4 exercício 1
    Configuração das pastas a partir do exercício do dia anterior.

---
+++ dia4/app/src/form.js
@@ -0,0 +1,68 @@
+
+
+const entrada = document.querySelector('[data-js="input"]')
+
+
+entrada.addEventListener('input',(txt)=>
+{
+  var words = txt.target.value.toLowerCase().split(' ')
+
+
+  words.forEach((word,index) =>
+  {
+    if(word == 'de' || word == 'da' || word == 'do' || word == 'dos' )
+    {
+      words[index] = word.toLowerCase()
+    }
+    else
+    {
+      words[index] = word.charAt(0).toUpperCase() + word.substring(1).toLowerCase()
+    }
+  });
+
+
+txt.target.value = words.join(' ')
+
+})
+
+const colors = new Map
+([
+  ['vermelho', '#AB0000'],
+  ['amarelo', '#FFC80D'],
+  ['verde', '#22B14B'],
+  ['laranja', '#FF7E26'],
+  ['azul', '#3F47CC']
+]);
+
+
+const indexform = document.querySelector('[data-js="form"]')
+const formselect = document.createElement('select')
+
+const colorbox = document.createElement('div')
+colorbox.style.display = 'flex'
+
+colors.forEach((key,value)=>
+{
+  const option = document.createElement('option')
+  option.value = key
+  option.textContent = value
+  formselect.appendChild(option)
+})
+
+formselect.addEventListener('change', (e)=>
+{
+  colorbox.innerHTML = ''
+  Array.from(e.target.selectedOptions).map(option =>
+    {
+      const div = document.createElement('div')
+      div.style.width = '100px'
+      div.style.height = '100px'
+      div.style.background = option.value
+      colorbox.appendChild(div)
+    })
+})
+
+
+formselect.setAttribute('multiple', '')
+indexform.appendChild(formselect)
+document.body.appendChild(colorbox)
