04e1b9f9d6993f5c96327b8ed1c4c4aaf02a74c6
Yuri Meitzen <31018558+Yurimeitzen0@users.noreply.github.com>
2021-08-22 14:48:40 -0300

    Dia 5 exercício de tipagem
    Segui o Daciuk realizando os ajustes durante a live, entendi boa parte, mas teria dificuldade pra fazer sozinho.

--- dia5/src/main.ts
+++ dia5/src/main.ts
@@ -2,20 +2,28 @@ import './style.css'
 import { get, post, del } from './http'
 
 const url = 'http://localhost:3333/cars'
-const form = document.querySelector('[data-js="cars-form"]')
-const table = document.querySelector('[data-js="table"]')
+const form = document.querySelector<HTMLFormElement>('[data-js="cars-form"]')!
+const table = document.querySelector<HTMLTableElement>('[data-js="table"]')!
 
-const getFormElement = (event) => (elementName) => {
-  return event.target.elements[elementName]
+type GetFormElement = (target:HTMLFormElement) => (elementName: string)=> HTMLInputElement
+const getFormElement: GetFormElement  = (target) => (elementName) =>
+{
+  return target[elementName]
 }
 
-const elementTypes = {
+const elementTypes =
+{
   image: createImage,
   text: createText,
   color: createColor,
 }
-
-function createImage (data) {
+type CreateImage =
+{
+  src: string
+  alt: string
+}
+function createImage (data: CreateImage)
+{
   const td = document.createElement('td')
   const img = document.createElement('img')
   img.src = data.src
@@ -25,13 +33,15 @@ function createImage (data) {
   return td
 }
 
-function createText (value) {
+function createText (value: string)
+{
   const td = document.createElement('td')
   td.textContent = value
   return td
 }
 
-function createColor (value) {
+function createColor (value: string)
+{
   const td = document.createElement('td')
   const div = document.createElement('div')
   div.style.width = '100px'
@@ -41,12 +51,18 @@ function createColor (value) {
   return td
 }
 
-form.addEventListener('submit', async (e) => {
+form.addEventListener('submit', async (e) =>
+{
   e.preventDefault()
-  const getElement = getFormElement(e)
+
+  const target = e.target as HTMLFormElement
+  if(!target)
+  return
+  const getElement = getFormElement(target)
+  const image = getElement('image')
 
   const data = {
-    image: getElement('image').value,
+    image: image.value,
     brandModel: getElement('brand-model').value,
     year: getElement('year').value,
     plate: getElement('plate').value,
@@ -67,25 +83,41 @@ form.addEventListener('submit', async (e) => {
 
   createTableRow(data)
 
-  e.target.reset()
+  target.reset()
   image.focus()
 })
-
-function createTableRow (data) {
+type TableRowData =
+{
+  image: string
+  brandModel: string
+  year: string
+  plate: string
+  color: string
+}
+function createTableRow (data: TableRowData)
+{
   const elements = [
     { type: 'image', value: { src: data.image, alt: data.brandModel } },
     { type: 'text', value: data.brandModel },
     { type: 'text', value: data.year },
     { type: 'text', value: data.plate },
     { type: 'color', value: data.color }
-  ]
+  ] as const
 
   const tr = document.createElement('tr')
   tr.dataset.plate = data.plate
 
-  elements.forEach(element => {
-    const td = elementTypes[element.type](element.value)
-    tr.appendChild(td)
+  elements.forEach(element =>
+  {
+    let td
+    if (element.type === 'image')
+      td = elementTypes.image(element.value)
+    if (element.type === 'text')
+      td = elementTypes.text(element.value)
+    if (element.type === 'color')
+      td = elementTypes.color(element.value)
+    if(td)
+      tr.appendChild(td)
   })
 
   const button = document.createElement('button')
@@ -99,8 +131,11 @@ function createTableRow (data) {
   table.appendChild(tr)
 }
 
-async function handleDelete (e) {
-  const button = e.target
+async function handleDelete (e: MouseEvent)
+{
+  const button = e.target as HTMLButtonElement
+  if(!button)
+  return
   const plate = button.dataset.plate
 
   const result = await del(url, { plate })
@@ -111,7 +146,13 @@ async function handleDelete (e) {
   }
 
   const tr = document.querySelector(`tr[data-plate="${plate}"]`)
-  table.removeChild(tr)
+  if(tr)
+  {
+    table.removeChild(tr)
+
+
+  }
+
   button.removeEventListener('click', handleDelete)
 
   const allTrs = table.querySelector('tr')
@@ -120,11 +161,12 @@ async function handleDelete (e) {
   }
 }
 
-function createNoCarRow () {
+function createNoCarRow ()
+{
   const tr = document.createElement('tr')
   const td = document.createElement('td')
   const thsLength = document.querySelectorAll('table th').length
-  td.setAttribute('colspan', thsLength)
+  td.setAttribute('colspan', String(thsLength))
   td.textContent = 'Nenhum carro encontrado'
 
   tr.dataset.js = 'no-content'
@@ -132,7 +174,8 @@ function createNoCarRow () {
   table.appendChild(tr)
 }
 
-async function main () {
+async function main ()
+{
   const result = await get(url)
 
   if (result.error) {
